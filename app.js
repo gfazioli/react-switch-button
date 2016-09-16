b7f0dbdf70efb168ca8edf37d6abb767958010e0
John Fazioli <john.fazioli@namecheap.com>
2016-09-16 16:03:34 +0200

    release 2.0.0

---
+++ app.js
@@ -0,0 +1,94 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var React = require('react'),
+    ReactDom = require('react-dom'),
+    SwitchButton = require('react-switch-button');
+
+var App = React.createClass({
+  displayName: 'App',
+
+  render: function render() {
+
+    var code = [{
+      label: "Simple usage",
+      code: '<SwitchButton name="switch-1" />', object: React.createElement(SwitchButton, { name: 'switch-1' })
+    }, {
+      label: "Set initial status",
+      code: '<SwitchButton name="switch-2" defaultChecked={true} />',
+      object: React.createElement(SwitchButton, { name: 'switch-2',
+        defaultChecked: true })
+    }, {
+      label: "Add left label",
+      code: '<SwitchButton name="switch-3" label="Click me" defaultChecked={true} />',
+      object: React.createElement(SwitchButton, { name: 'switch-3',
+        label: 'Click me',
+        defaultChecked: true })
+    }, {
+      label: "Add right label",
+      code: '<SwitchButton name="switch-4" labelRight="Click me" defaultChecked={true} />',
+      object: React.createElement(SwitchButton, { name: 'switch-4',
+        labelRight: 'Click me',
+        defaultChecked: true })
+    }, {
+      label: "Add left and right label",
+      code: '<SwitchButton name="switch-5" label="Both" label_right="Click me" defaultChecked={true} />',
+      object: React.createElement(SwitchButton, { name: 'switch-5',
+        label: 'Both',
+        labelRight: 'Click me',
+        defaultChecked: true })
+    }, {
+      label: "Disabled when On",
+      code: '<SwitchButton name="switch-6" label="Disabled" disabled={true} defaultChecked={true} />',
+      object: React.createElement(SwitchButton, { name: 'switch-6',
+        label: 'Disabled',
+        disabled: true,
+        defaultChecked: true })
+    }, {
+      label: "Disabled when Off",
+      code: '<SwitchButton name="switch-7" label="Disabled" disabled={true} />',
+      object: React.createElement(SwitchButton, { name: 'switch-7',
+        label: 'Disabled',
+        disabled: true })
+    }];
+
+    var rows = code.map(function (o, i) {
+
+      return React.createElement(
+        'div',
+        { className: 'example',
+          key: i },
+        React.createElement(
+          'h3',
+          null,
+          o.label
+        ),
+        React.createElement(
+          'pre',
+          null,
+          o.code
+        ),
+        React.createElement(
+          'div',
+          { className: 'result' },
+          o.object
+        )
+      );
+    });
+
+    return React.createElement(
+      'div',
+      { className: 'examples' },
+      React.createElement(
+        'h1',
+        null,
+        'Say Hello, React Switch Button'
+      ),
+      rows
+    );
+  }
+});
+
+ReactDom.render(React.createElement(App, null), document.getElementById('app'));
+
+},{"react":undefined,"react-dom":undefined,"react-switch-button":undefined}]},{},[1]);
---
+++ bundle.js
@@ -0,0 +1,104 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"react-switch-button":[function(require,module,exports){
+'use strict';
+
+var React = require('react');
+
+var SwitchButton = React.createClass({
+
+  // Display name
+  displayName: 'Switch Button',
+
+  // Version
+  version: '2.0.0',
+
+  /**
+   * The props types.
+   */
+  propTypes: {
+    id: React.PropTypes.string,
+    name: React.PropTypes.string,
+    title: React.PropTypes.string,
+    label: React.PropTypes.string,
+    labelRight: React.PropTypes.string,
+    defaultChecked: React.PropTypes.bool,
+    disabled: React.PropTypes.bool,
+    theme: React.PropTypes.string,
+    checked: React.PropTypes.string,
+    onChange: React.PropTypes.func
+  },
+
+  /**
+   * Default props.
+   *
+   * @returns {{id: string, name: string, title: string, label: string, labelRight: string, defaultChecked: string, theme: string, checked: null, onChange: *}}
+   */
+  getDefaultProps: function getDefaultProps() {
+    return {
+      id: '',
+      name: 'switch-button',
+      title: '',
+      label: '',
+      labelRight: '',
+      disabled: false,
+      defaultChecked: false,
+      theme: 'rsbc-switch-button-flat-round',
+      checked: null,
+      onChange: this.handleChange
+    };
+  },
+
+  // Handle change
+  handleChange: function handleChange() {
+    // Override
+  },
+
+  /**
+   * Render Switch Button control
+   *
+   * @returns {XML}
+   */
+  render: function render() {
+    var id = undefined,
+        label = undefined,
+        labelRight = undefined;
+
+    if (this.props.id == '' && this.props.name != '') {
+      id = this.props.name;
+    }
+
+    if (this.props.label != '') {
+      label = React.createElement(
+        'label',
+        { htmlFor: id },
+        this.props.label
+      );
+    }
+
+    if (this.props.labelRight != '') {
+      labelRight = React.createElement(
+        'label',
+        { htmlFor: id },
+        this.props.labelRight
+      );
+    }
+
+    return React.createElement(
+      'div',
+      { className: 'rsbc-switch-button ' + this.props.theme + (this.props.disabled ? " disabled" : "") },
+      label,
+      React.createElement('input', { onChange: this.props.onChange,
+        defaultChecked: this.props.defaultChecked,
+        disabled: this.props.disabled,
+        id: id, name: this.props.name,
+        type: 'checkbox',
+        value: '1' }),
+      React.createElement('label', { htmlFor: id }),
+      labelRight
+    );
+  }
+
+});
+
+module.exports = SwitchButton;
+
+},{"react":undefined}]},{},[]);
